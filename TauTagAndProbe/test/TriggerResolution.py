5418655de620821ef0a615d5cd614e4d72c11d22
Simone Bologna <smnbologna@gmail.com>
2016-07-07 18:52:50 +0200

    Create 2D plot for resolution vs dr

--- TauTagAndProbe/test/TriggerResolution.py
+++ TauTagAndProbe/test/TriggerResolution.py
@@ -10,6 +10,10 @@ l1tResolution1 = TH1I("L1TriggerReso1", "L1T resolution for offline pt between 4
 hltResolution1 = TH1I("HLTriggerReso1", "HLT resolution for offline pt between 40 and 60 GeV", 100, -100, 100)
 l1tResolution2 = TH1I("L1TriggerReso2", "L1T resolution for offline pt between 60 and 100 GeV", 100, -100, 100)
 hltResolution2 = TH1I("HLTriggerReso2", "HLT resolution for offline pt between 60 and 100 GeV", 100, -100, 100)
+#x = resolution
+#y = R
+l1tResolutionVsDR = TH2I("L1TriggerResoVsDR", "L1T resolution vs DR", 100, -100, 100, 30, -0.1, 0.2)
+hltResolutionVsDR = TH2I("HLTriggerResoVsDR", "HLT resolution vs DR", 100, -100, 100, 30, -0.1, 0.2)
 
 for iEv in range (0, tree.GetEntries()):
     tree.GetEntry(iEv)
@@ -18,6 +22,8 @@ for iEv in range (0, tree.GetEntries()):
     ptHLT = tree.hltPt
     deltaPtL1T = ptOffline - ptL1T
     deltaPtHLT = ptOffline - ptHLT
+    deltaR = ((tree.tauEta - tree.hltEta)**2 + (tree.tauPhi - tree.hltPhi)**2)**0.5
+    
     if (ptOffline > 40)  and (ptOffline < 60):
         l1tResolution1.Fill(deltaPtL1T)
         hltResolution1.Fill(deltaPtHLT)
@@ -25,29 +31,30 @@ for iEv in range (0, tree.GetEntries()):
         l1tResolution2.Fill(deltaPtL1T)
         hltResolution2.Fill(deltaPtHLT)
 
-c1 = TCanvas("canvas1")
-c1.cd()
-l1tResolution1.GetXaxis().SetTitle("ptOffline - ptL1T");
-l1tResolution1.Draw()
+    hltResolutionVsDR.Fill(deltaPtHLT, deltaR)
+  
+l1tResolution1.GetXaxis().SetTitle("ptOffline - ptL1T")
+#l1tResolution1.Draw()
+#c1.Update()
+#c1.Print("l1tReso_40_60.pdf", "pdf")
+l1tResolution2.GetXaxis().SetTitle("ptOffline - ptL1T")
+#l1tResolution2.Draw()
+#c1.Update()
+#c1.Print("l1tReso_60_100.pdf", "pdf")
+hltResolution1.GetXaxis().SetTitle("ptOffline - ptHLT")
+#hltResolution1.Draw()
+#c1.Update()
+#c1.Print("hltReso_40_60.pdf", "pdf")
+hltResolution2.GetXaxis().SetTitle("ptOffline - ptHLT")
+#hltResolution2.Draw()
+#c1.Update()
+#c1.Print("hltReso_60_100.pdf", "pdf")
+
+hltResolutionVsDR.GetXaxis().SetTitle("ptOffline - ptHLT")
+hltResolutionVsDR.GetYaxis().SetTitle("Delta R")
+hltResolutionVsDR.Draw("COLZ")
 c1.Update()
-c1.Print("l1tReso_40_60.pdf", "pdf")
-c2 = TCanvas("canvas2")
-c2.cd()
-l1tResolution2.Draw()
-l1tResolution2.GetXaxis().SetTitle("ptOffline - ptL1T");
-c2.Update()
-c2.Print("l1tReso_60_100.pdf", "pdf")
-c3 = TCanvas("canvas3")
-c3.cd()
-hltResolution1.GetXaxis().SetTitle("ptOffline - ptHLT");
-hltResolution1.Draw()
-c3.Update()
-c3.Print("hltReso_40_60.pdf", "pdf")
-c4 = TCanvas("canvas4")
-c4.cd()
-hltResolution2.GetXaxis().SetTitle("ptOffline - ptHLT");
-hltResolution2.Draw()
-c4.Update()
-c1.Print("hltReso_60_100.pdf", "pdf")
+c1.Print("hltResoVsDR.pdf", "pdf")
+
 
 raw_input()
