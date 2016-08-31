b1b65d033c5c45713248b2d0c177c7b1ca666508
Simone Bologna <smnbologna@gmail.com>
2016-08-31 11:40:11 +0200

    Added Barrel vs Endcap fitter

---
+++ TauTagAndProbe/test/fitter/results/plot_turnOn_Barrel_vs_Endcap_30GeV.py
@@ -0,0 +1,74 @@
+import ROOT
+import TurnOnPlot_DATA as TurnOnPlot
+
+
+plots = []
+plots.append(TurnOnPlot.TurnOnPlot())
+plots[-1].name = "turnOn_Barrel_EndCap_30GeV"
+plots[-1].xRange = (10,109.9)
+#plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
+plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
+
+#EE
+plots.append(TurnOnPlot.TurnOnPlot())
+plots[-1].name = "turnOn_Barrel_EndCap_30GeV_iso"
+plots[-1].xRange = (10,109.9)
+#plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
+plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
+
+#open 30GeV non iso plot
+inputFile = ROOT.TFile.Open("L1T_TurnOns.root")
+
+histo_EB = inputFile.Get("histo_Stage2_Barrel_vs_Pt_30GeV")
+histo_EB.__class__ = ROOT.RooHist
+histo_EE = inputFile.Get("histo_Stage2_Endcaps_vs_Pt_30GeV")
+histo_EE.__class__ = ROOT.RooHist
+
+
+fit_EB   = inputFile.Get("fit_Stage2_Barrel_vs_Pt_30GeV")
+fit_EB.__class__ = ROOT.RooCurve
+fit_EE   = inputFile.Get("fit_Stage2_Endcaps_vs_Pt_30GeV")
+fit_EE.__class__ = ROOT.RooCurve
+
+turnon_EB = TurnOnPlot.TurnOn(Name="Stage2_EB_noIso", Histo=histo_EB, Fit=fit_EB,
+                                    MarkerColor=ROOT.kBlack, MarkerStyle=20, LineColor=ROOT.kBlack,LineStyle=1,
+                                    Legend="Barrel")
+
+turnon_EE = TurnOnPlot.TurnOn(Name="Stage2_EE_noIso", Histo=histo_EE, Fit=fit_EE,
+                                   MarkerColor=ROOT.kRed, MarkerStyle=20, LineColor=ROOT.kRed,LineStyle=1,
+                                   Legend="Endcaps")
+
+
+plots[0].addTurnOn(turnon_EB)
+plots[0].addTurnOn(turnon_EE)
+
+'''
+histo_EB = inputFile.Get("histo_Stage2_Barrel_vs_Pt_30GeV_iso")
+histo_EB.__class__ = ROOT.RooHist
+histo_EE = inputFile.Get("histo_Stage2_Endcaps_vs_Pt_30GeV_iso")
+histo_EE.__class__ = ROOT.RooHist
+
+
+fit_EB   = inputFile.Get("fit_Stage2_Barrel_vs_Pt_30GeV_iso")
+fit_EB.__class__ = ROOT.RooCurve
+fit_EE   = inputFile.Get("fit_Stage2_Endcaps_vs_Pt_30GeV_iso")
+fit_EE.__class__ = ROOT.RooCurve
+
+turnon_EB = TurnOnPlot.TurnOn(Name="Stage2_EB_Iso", Histo=histo_EB, Fit=fit_EB,
+                                    MarkerColor=ROOT.kRed, MarkerStyle=20, LineColor=ROOT.kRed,LineStyle=1,
+                                    Legend="Barrel")
+
+turnon_EE = TurnOnPlot.TurnOn(Name="Stage2_EE_Iso", Histo=histo_EE, Fit=fit_EE,
+                                   MarkerColor=ROOT.kRed, MarkerStyle=20, LineColor=ROOT.kRed,LineStyle=1,
+                                   Legend="Endcaps")
+
+
+plots[1].addTurnOn(turnon_EB)
+plots[1].addTurnOn(turnon_EE)
+'''
+
+canvas = []
+for plot in plots:
+    canvas.append(plot.plot())
+
+inputFile.Close()
