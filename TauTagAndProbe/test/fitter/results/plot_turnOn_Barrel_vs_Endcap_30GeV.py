38d2b1476ea2afabae65e0530e5ac8f6e3b98fa3
Simone Bologna <smnbologna@gmail.com>
2016-08-31 18:07:52 +0200

    Done L1T plots

--- TauTagAndProbe/test/fitter/results/plot_turnOn_Barrel_vs_Endcap_30GeV.py
+++ TauTagAndProbe/test/fitter/results/plot_turnOn_Barrel_vs_Endcap_30GeV.py
@@ -3,27 +3,26 @@ import TurnOnPlot_DATA as TurnOnPlot
 
 
 plots = []
-plots.append(TurnOnPlot.TurnOnPlot())
+plots.append(TurnOnPlot.TurnOnPlot(TriggerName="L1 Threshold: 28 GeV iso + non-iso"))
 plots[-1].name = "turnOn_Barrel_EndCap_30GeV"
 plots[-1].xRange = (10,109.9)
 #plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
 plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
 
 #EE
-plots.append(TurnOnPlot.TurnOnPlot())
+plots.append(TurnOnPlot.TurnOnPlot(TriggerName="L1 Threshold: 28 GeV iso"))
 plots[-1].name = "turnOn_Barrel_EndCap_30GeV_iso"
 plots[-1].xRange = (10,109.9)
 #plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
 plots[-1].legendPosition = (0.6,0.2,0.9,0.4)
 
-#open 30GeV non iso plot
+#open turn on file
 inputFile = ROOT.TFile.Open("L1T_TurnOns.root")
 
 histo_EB = inputFile.Get("histo_Stage2_Barrel_vs_Pt_30GeV")
 histo_EB.__class__ = ROOT.RooHist
 histo_EE = inputFile.Get("histo_Stage2_Endcaps_vs_Pt_30GeV")
 histo_EE.__class__ = ROOT.RooHist
-
 
 fit_EB   = inputFile.Get("fit_Stage2_Barrel_vs_Pt_30GeV")
 fit_EB.__class__ = ROOT.RooCurve
@@ -42,12 +41,11 @@ turnon_EE = TurnOnPlot.TurnOn(Name="Stage2_EE_noIso", Histo=histo_EE, Fit=fit_EE
 plots[0].addTurnOn(turnon_EB)
 plots[0].addTurnOn(turnon_EE)
 
-'''
+
 histo_EB = inputFile.Get("histo_Stage2_Barrel_vs_Pt_30GeV_iso")
 histo_EB.__class__ = ROOT.RooHist
 histo_EE = inputFile.Get("histo_Stage2_Endcaps_vs_Pt_30GeV_iso")
 histo_EE.__class__ = ROOT.RooHist
-
 
 fit_EB   = inputFile.Get("fit_Stage2_Barrel_vs_Pt_30GeV_iso")
 fit_EB.__class__ = ROOT.RooCurve
@@ -55,7 +53,7 @@ fit_EE   = inputFile.Get("fit_Stage2_Endcaps_vs_Pt_30GeV_iso")
 fit_EE.__class__ = ROOT.RooCurve
 
 turnon_EB = TurnOnPlot.TurnOn(Name="Stage2_EB_Iso", Histo=histo_EB, Fit=fit_EB,
-                                    MarkerColor=ROOT.kRed, MarkerStyle=20, LineColor=ROOT.kRed,LineStyle=1,
+                                    MarkerColor=ROOT.kBlack, MarkerStyle=20, LineColor=ROOT.kBlack,LineStyle=1,
                                     Legend="Barrel")
 
 turnon_EE = TurnOnPlot.TurnOn(Name="Stage2_EE_Iso", Histo=histo_EE, Fit=fit_EE,
@@ -65,10 +63,12 @@ turnon_EE = TurnOnPlot.TurnOn(Name="Stage2_EE_Iso", Histo=histo_EE, Fit=fit_EE,
 
 plots[1].addTurnOn(turnon_EB)
 plots[1].addTurnOn(turnon_EE)
-'''
 
 canvas = []
 for plot in plots:
     canvas.append(plot.plot())
 
+
 inputFile.Close()
+
+raw_input()
